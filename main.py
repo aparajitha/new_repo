69d12bfb9b03d9ca3446e259bf3978783e3f76a0
Aparajitha <aparajitha88@gmail.com>
2024-11-07 15:34:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from math_operations import Maths
+class main:
+    m = Maths(16, 4)
+    print("Sum of 16 & 4 = " +str(m.add_nums()))
+    print("Difference of 16 & 4 = " +str(m.substract_nums()))
+    print("Multiplication of 16 & 4 = " +str(m.multiply_nums()))
+    print("Division of 16 & 4 = " +str(m.divide_nums()))
+    
+    
